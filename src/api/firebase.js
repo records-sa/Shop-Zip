e363c1cf35aaa784024593d8bbb7e11a6326198e
Seoa Kim <seoa521@gmail.com>
2023-11-23 17:00:42 +0900

    [feat] 구글 로그인, 로그아웃 기능 구현

---
+++ src/api/firebase.js
@@ -0,0 +1,45 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+export async function login() {
+  return signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      // const credential = GoogleAuthProvider.credentialFromResult(result);
+      // const token = credential.accessToken;
+      // console.log("크리덴셜", credential);
+      // console.log("토큰", token);
+      const user = result.user;
+      console.log("유저", user);
+      return user;
+    })
+    .catch(console.error);
+}
+
+export async function logout() {
+  return signOut(auth).then(() => null);
+}
+
+export function onUserStateChange(callback) {
+  onAuthStateChanged(auth, (user) => {
+    callback(user);
+  });
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { BsShop, BsPencil } from "react-icons/bs";
+import { login, logout, onUserStateChange } from "../api/firebase";
 
 function Navbar() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    onUserStateChange((user) => {
+      console.log(user);
+      setUser(user);
+    });
+  }, []);
+
+  const handleLogin = () => {
+    login().then(setUser);
+  };
+
+  const handleLogout = () => {
+    logout().then(setUser);
+  };
+
   return (
     <header className="flex justify-between border-b border-gray-300 p-2">
-      <Link to="/" className="flex items-center text-4xl text-brand">
+      <Link to="/" className="flex items-center text-4xl gap-2 text-brand">
         <BsShop />
         <h1>Shop Zip</h1>
       </Link>
@@ -15,7 +33,8 @@ function Navbar() {
         <Link to="/products/new" className="text-xl">
           <BsPencil />
         </Link>
-        <button>Login</button>
+        {!user && <button onClick={handleLogin}>Login</button>}
+        {user && <button onClick={handleLogout}>Logout</button>}
       </nav>
     </header>
   );
